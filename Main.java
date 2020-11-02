9d3844b05a3b9c212e0f5a8360cde5cf49b62acf
shibilm22 <shibil.mandawe@patelhs.org>
2020-11-02 21:21:30 +0000

    added for loop 1

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
   public static void main(String[] args) {
   Sting []  chips = {"Takis", "Doritos", "Lays", "Cheetos", "Funyuns", "Ruffles", "Pringles"};
+
+  for (int i=0; i<6; i++){
+    System.out.println(chips[i]);
+  }
+
   
   }
 }
